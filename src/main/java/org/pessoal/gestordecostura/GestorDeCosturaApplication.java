fea62a10c5559cff7a5b9d80993fcbcf35a199d2
BorgesGh <ghabrielbc@gmail.com>
2024-01-21 13:32:52 -0300

    primeiro commit

---
+++ src/main/java/org/pessoal/gestordecostura/GestorDeCosturaApplication.java
@@ -0,0 +1,13 @@
+package org.pessoal.gestordecostura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeCosturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorDeCosturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pessoal/gestordecostura/GestorDeCosturaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pessoal.gestordecostura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeCosturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
