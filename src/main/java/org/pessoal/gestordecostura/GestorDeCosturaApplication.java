a8a77bbd8ce0e0054be8fdf9fd6c3a51a7dddc03
BorgesGh <ghabrielbc@gmail.com>
2024-01-23 14:59:38 -0300

    configuraçãoes de banco

--- src/main/java/org/pessoal/gestordecostura/GestorDeCosturaApplication.java
+++ src/main/java/org/pessoal/gestordecostura/GestorDeCosturaApplication.java
@@ -2,6 +2,7 @@ package org.pessoal.gestordecostura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class GestorDeCosturaApplication {
--- src/main/resources/db.migration/V01__criacao_das_tabelas.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Cliente(
-    id_cliente integer serial primary key,
-    nome_cliente varchar(30),
-    telefone varchar(11)
-);
